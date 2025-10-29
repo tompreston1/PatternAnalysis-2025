41422e806f24f2541e58185e7d19328aaefe25f7
tompreston1 <thomas.preston2305@gmail.com>
2025-10-29 14:28:59 +1000

    dataset transform update

--- recognition/dataset.py
+++ recognition/dataset.py
@@ -76,8 +76,9 @@ def get_transforms(train=True, size=224, rgb=True):
         return A.Compose([
             A.Resize(size, size),
             A.HorizontalFlip(p=0.5),
-            A.RandomBrightnessContrast(p=0.25),
-            A.ShiftScaleRotate(shift_limit=0.02, scale_limit=0.05, rotate_limit=10, border_mode=cv2.BORDER_REFLECT_101, p=0.25),
+            A.RandomBrightnessContrast(brightness_limit=0.15, contrast_limit=0.15, p=0.3),
+            A.ShiftScaleRotate(shift_limit=0.02, scale_limit=0.05, rotate_limit=8,
+                               border_mode=cv2.BORDER_REFLECT_101, p=0.25),
             A.Normalize(mean=mean, std=std),
             ToTensorV2(),
         ])
@@ -87,4 +88,5 @@ def get_transforms(train=True, size=224, rgb=True):
             A.Normalize(mean=mean, std=std),
             ToTensorV2(),
         ])
+
 
